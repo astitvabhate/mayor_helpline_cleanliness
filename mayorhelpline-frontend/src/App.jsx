013120e0a25dd4f90e655b73d3d2c36b19e422c6
astitvabhate <bhateastitva204@gmail.com>
2025-07-23 20:41:01 +0530

    build: rebuild for Render deploy with router fix

--- mayorhelpline-frontend/src/App.jsx
+++ mayorhelpline-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/admin/admin-dashboard";
 import AdminLogin from "./pages/admin/admin-login";
 import PrivateRoute from "./components/PrivateRoute";
@@ -7,21 +7,19 @@ import Home from "./pages/home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/client/report" element={<ReportForm />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route
-          path="/admin/dashboard"
-          element={
-            <PrivateRoute>
-              <AdminDashboard />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/client/report" element={<ReportForm />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <PrivateRoute>
+            <AdminDashboard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
   );
 }
 
